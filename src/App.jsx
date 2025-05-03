5646804b28498ab521e3337b867b397c53697fb3
Michał Skupiński <m.skupinski2404@gmail.com>
2025-05-04 00:54:14 +0200

    xd

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
         <Sidebar />
         <div className='PageContent Box'>
           <Routes>
-            <Route path="/skubi_snackzz/" element={<Home />} />
-            <Route path="/skubi_snackzz/music" element={<Music />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/music" element={<Music />} />
           </Routes>
         </div>
       </div>
